377ad47fa0314598efdafae3d4c36da57783900d
THAO <thaopn@greenglobal.vn>
2015-06-15 21:53:23 +0700

    Initial Commit

---
+++ TheColors/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TheColors
+//
+//  Created by GreenGlobal on 6/15/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TheColors/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TheColors
+//
+//  Created by GreenGlobal on 6/15/15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
