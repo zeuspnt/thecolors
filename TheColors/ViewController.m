47a67d24b20c8d42ed9d8dfd0b983ed16807b1c2
THAO <thaopn@greenglobal.vn>
2015-06-15 22:44:39 +0700

    Init

--- TheColors/ViewController.m
+++ TheColors/ViewController.m
@@ -8,7 +8,15 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    
+    __weak IBOutlet UIProgressView *_progressBar;
+    __weak IBOutlet UILabel *_lblTheColors;
+    
+    NSTimer *_timer;
+    NSArray *_arrayColors, *_arrayColorsName;
+    BOOL _result;
+}
 
 @end
 
@@ -18,6 +26,55 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    
+    
+    _arrayColors = @[[UIColor blackColor],[UIColor grayColor],[UIColor redColor],[UIColor greenColor],[UIColor blueColor],[UIColor cyanColor],[UIColor yellowColor],[UIColor magentaColor],[UIColor orangeColor],[UIColor purpleColor],[UIColor brownColor]];
+    
+    _arrayColorsName = @[@"Black",@"Gray",@"Red",@"Green",@"Blue",@"Cyan",@"Yellow",@"Magenta",@"Orange",@"Purple",@"Brown"];
+    NSLog(@"%d - %d", _arrayColorsName.count, _arrayColors.count);
+    
+    [self startGame];
+}
+
+- (void)randomColor:(BOOL)right{
+    if (right) {
+        NSInteger i = arc4random() % _arrayColors.count;
+        _lblTheColors.textColor = [_arrayColors objectAtIndex:i];
+        _lblTheColors.text = [_arrayColorsName objectAtIndex:i];
+        _result = YES;
+    } else {
+        NSInteger i = arc4random() % _arrayColors.count;
+        NSInteger j = arc4random() % _arrayColorsName.count;
+        _lblTheColors.textColor = [_arrayColors objectAtIndex:i];
+        _lblTheColors.text = [_arrayColorsName objectAtIndex:j];
+        if (i == j) {
+            _result = YES;
+        } else {
+            _result = NO;
+        }
+    }
+}
+
+- (void)tick{
+    _progressBar.progress -=0.01f;
+    if (_progressBar.progress == 0) {
+        //Game Over
+        [self endGame];
+    }
+}
+
+- (void)startGame{
+    _progressBar.progress = 1.f;
+    _timer = [NSTimer scheduledTimerWithTimeInterval:0.01f target:self selector:@selector(tick) userInfo:nil repeats:YES];
+    [self randomColor:NO];
+}
+
+- (void)endGame{
+    _progressBar.progress = 0;
+    [_timer invalidate];
+    _timer = nil;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +83,8 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)btnYesTapped:(id)sender {
+}
+- (IBAction)btnNoTapped:(id)sender {
+}
 @end
